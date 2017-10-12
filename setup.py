28dde5e65368757a412bceb1265804c80e906481
mattwthompson <matt.thompson@vanderbilt.edu>
2017-10-12 15:59:31 -0500

    Add setup.py and requirements.txt

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='scattering',
+      version='0.0',
+      description='Compute scattering functions',
+      url='http://github.com/mattwthompson/scattering',
+      author='Matthew W. Thompson',
+      author_email='matt.thompson@vanderbilt.edu',
+      license='MIT',
+      packages=['scattering'],
+      zip_safe=False)
