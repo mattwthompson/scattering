fb465f2e8bc3c3c4f9e5550a573aa3ab7a2f024c
Matthew W. Thompson <matt.thompson@vanderbilt.edu>
2019-02-25 17:36:33 -0600

    Add X-ray form factor for water

--- scattering/utils/constants.py
+++ scattering/utils/constants.py
@@ -13,12 +13,20 @@ def get_form_factor(name=None):
     return form_factor
 
 def get_form_factor_water(element_name=None):
-    elem = Element.getBySymbol(element_name)
-
-    if elem.name == 'oxygen':
-        return float(1/3)
-    elif elem.name == 'oxygen':
-        return float(9 + 1/3)
+    if element_name is not None:
+        elem = Element.getBySymbol(element_name)
     else:
-        warnings.warn('Found an element not in water')
-        return None
+        riase ValueError('Need an element')
+
+    if elem.atomic_number not in [1, 8]:
+        raise ValueError('')
+
+    if elem.atomic_number == 1:
+        form_factor = float(1/3)
+    elif elem.atomic_number == 8:
+        form_factor = float(9 + 1/3)
+    else:
+        raise ValueError('Found an element not in water')
+        form_factor = None
+
+    return form_factor
