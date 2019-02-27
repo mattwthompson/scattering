8c4268fd44a0cb8a4b3167dd317c613a574b21e4
Matthew W. Thompson <matt.thompson@vanderbilt.edu>
2019-02-27 10:58:35 -0600

    Make heatmap prettier

--- examples/van-hove-function.py
+++ examples/van-hove-function.py
@@ -10,13 +10,13 @@ from scattering.utils.features import find_local_maxima
 trj = md.load(get_fn('10fs.xtc'),
               top=get_fn('10fs.gro'),
               stride=1,
-              )[:1000]
+              )[:10000]
 
 chunk_length = 200
 selection1 = 'name O'
 selection2 = 'name O'
 
-r, t, g_r_t = compute_van_hove(trj, chunk_length)
+r, t, g_r_t = compute_van_hove(trj=trj, chunk_length=chunk_length, water=True)
 
 dt = get_dt(trj)
 
@@ -25,18 +25,20 @@ np.savetxt('vhf.txt', g_r_t, header='# Van Hove Function, dt: {} fs, dr: {}'.for
     dt,
     np.unique(np.round(np.diff(trj.time), 6))[0],
 ))
-np.savetxt('r.txt', r)
-np.savetxt('t.txt', t)
+np.savetxt('r.txt', r, header='# Times, ps')
+np.savetxt('t.txt', t, header='# Positions, nm')
 
 
 fig, ax = plt.subplots()
 
 for j in range(chunk_length):
+    if j % 10 != 0 or j > 50:
+        continue
     r_max, g_r_max = find_local_maxima(r, g_r_t[j], r_guess=0.3)
     plt.plot(r_max, g_r_max, 'k.')
     r_max, g_r_max = find_local_maxima(r, g_r_t[j], r_guess=0.45)
     plt.plot(r_max, g_r_max, 'k.')
-    plt.plot(r, g_r_t[j], label='{} ps'.format(t[j]))
+    plt.plot(r, g_r_t[j], label='{:.3f} ps'.format(t[j]))
 
 
 # Save output to text files
@@ -55,12 +57,11 @@ plt.savefig('van-hove-function.pdf')
 
 fig, ax = plt.subplots()
 
-heatmap = ax.imshow(g_r_t[::-1]-1, vmin=-0.04, vmax=0.04, cmap='RdYlGn_r')
+heatmap = ax.imshow(g_r_t-1, vmin=-0.04, vmax=0.04, cmap='RdYlGn_r', origin='lower', extent=(r[0], r[-1], t[0], t[-1]))
+ax.grid(False)
+ax.set_xlabel(r'Radial position, $r$, $nm$')
+ax.set_ylabel(r'Time, $t$, $ps$')
+
 plt.colorbar(heatmap)
 
 fig.savefig('heatmap.pdf')
-
-# Save output to text files
-np.savetxt('vhf.txt', g_r_t)
-np.savetxt('r.txt', r)
-np.savetxt('t.txt', t)
