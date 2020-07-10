6606edd788e2e1fa87f56deb66aacbcdc5a211b2
Xiaobo Lin <xiaobolin11@outlook.com>
2020-07-10 11:44:42 -0500

    new function - scattering_factor_pair_test

--- scattering/scattering.py
+++ scattering/scattering.py
@@ -196,7 +196,114 @@ def structure_factor_pair(trj, pair, Q_range=(0.5, 50), n_points=1000, framewise
         S[i] = (num/(denom**2))
     return Q, S
 
+def structure_factor_pair_test(trj_full, trj_sep, pair, Q_range=(0.5, 50), n_points=1000, framewise_rdf=False, method='fz'):
+"""
+  trj_full is the trajectory with only IL, and trj_sep renamed the atoms in cation (using name of atom + 'ca') and renamed the
+  atoms in anion (using name of atom + 'an').
+  
+"""     
+    rho = np.mean(trj_full.n_atoms / trj_full.unitcell_volumes)
+    L = np.min(trj_full.unitcell_lengths)
 
+    top_full = trj_full.topology
+    top_sep = trj_sep.topology
+    
+    elements_i = set()
+    elements_j = set()
+    
+    for a in top_sep.atoms:
+        string = a.element.symbol
+        if string[-2:] == pair[0]:
+            elements_i.add((a.element))
+        if string[-2:] == pair[1]:  
+            elements_j.add((a.element))
+
+    Number_scale = dict()
+    
+    elements = set([a.element for a in top_full.atoms])
+
+    compositions = dict()
+    form_factors = dict()
+    rdfs = dict()
+
+    Q = np.logspace(np.log10(Q_range[0]),
+                    np.log10(Q_range[1]),
+                    num=n_points)
+    S = np.zeros(shape=(len(Q)))
+
+    for elem in elements:
+        compositions[elem.symbol] = len(top_full.select('element {}'.format(elem.symbol)))/trj_full.n_atoms
+        form_factors[elem.symbol] = elem.atomic_number
+    
+
+    for i, q in enumerate(Q):
+        num = 0
+        denom = 0
+
+        for elem in elements:
+            denom += compositions[elem.symbol] * form_factors[elem.symbol]
+
+        for (elem1, elem2) in it.product(elements, repeat=2):
+            e1 = elem1.symbol
+            e2 = elem2.symbol
+            
+            atomtype1 = e1[0]
+            atomtype2 = e2[0]
+
+            f_a = form_factors[e1]
+            f_b = form_factors[e2]
+
+            x_a = compositions[e1]
+            x_b = compositions[e2]
+            
+            length_1 = len(top_sep.select('element {}'.format(e1 + pair[0])))
+            length_2 = len(top_sep.select('element {}'.format(e2 + pair[1])))
+
+            
+            if (length_1 == 0) or (length_2 == 0):
+                integral = 0
+
+            else:
+                try:
+                    g_r = rdfs['{0}{1}'.format(e1, e2)]
+                    scale = Number_scale['{0}{1}'.format(e1, e2)]
+                except KeyError:
+                    pairs = top_sep.select_pairs(selection1='element {}'.format(e1 + pair[0]),
+                                             selection2='element {}'.format(e2 + pair[1]))
+                    if framewise_rdf:
+                        r, g_r = rdf_by_frame(trj_sep,
+                                             pairs=pairs,
+                                             r_range=(0, L / 2),
+                                             bin_width=0.001)
+                    else:
+                        r, g_r = md.compute_rdf(trj_sep,
+                                                pairs=pairs,
+                                                r_range=(0, L / 2),
+                                                bin_width=0.001)
+                    rdfs['{0}{1}'.format(e1, e2)] = g_r
+
+                    N_i = len(top_full.select('element {}'.format(e1)))
+                    N_j = len(top_full.select('element {}'.format(e2)))
+
+                    N_i_1 = len(top_sep.select('element {}'.format(e1+ pair[0])))
+                    N_i_2 = len(top_sep.select('element {}'.format(e2+ pair[1])))
+
+                    scale = N_i_1 *  N_i_2 / (N_i * N_j)
+                    Number_scale['{0}{1}'.format(e1, e2)] = scale
+
+                integral = simps(r ** 2 * (g_r - g_r[-1]) * np.sin(q * r) / (q * r), r) * scale
+#                 print(integral)
+
+            if method == 'al':
+                pre_factor = np.sqrt(x_a * x_b) * 4 * np.pi * rho
+                partial_sq = (integral*pre_factor) 
+                num += (f_a*f_b) * (partial_sq+1) * np.sqrt(x_a*x_b)
+            elif method == 'fz':
+                pre_factor = 4 * np.pi * rho
+                partial_sq = (integral*pre_factor) 
+                num += (x_a*f_a*x_b*f_b) * (partial_sq)
+        S[i] = (num/(denom**2))
+    return Q, S
 
 def compute_dynamic_rdf(trj):
     """Compute r_ij(t), the distance between atom j at time t and atom i and
