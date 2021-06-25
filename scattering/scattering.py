639b02d12a424d8bcc3bfa4bc305a28c30f3679c
Ray Matsumoto <ray.a.matsumoto@vanderbilt.edu>
2021-06-24 23:30:30 -0500

    Add other normalization factor for s(q)

--- scattering/scattering.py
+++ scattering/scattering.py
@@ -10,11 +10,12 @@ from scattering.utils.utils import get_dt
 from scattering.utils.constants import get_form_factor
 
 
-#__all__ = ['structure_factor', 'compute_partial_van_hove', 'compute_van_hove']
+# __all__ = ['structure_factor', 'compute_partial_van_hove', 'compute_van_hove']
 
 
-
-def structure_factor(trj, Q_range=(0.5, 50), n_points=1000, framewise_rdf=False, weighting_factor='fz'):
+def structure_factor(
+    trj, Q_range=(0.5, 50), n_points=1000, framewise_rdf=False, weighting_factor="fz"
+):
     """Compute the structure factor through a fourier transform of
     the radial distribution function.
 
@@ -37,8 +38,8 @@ def structure_factor(trj, Q_range=(0.5, 50), n_points=1000, framewise_rdf=False,
     framewise_rdf : boolean, default=False
         If True, computes the rdf frame-by-frame. This can be useful for
         managing memory in large systems.
-    weighting_factor : string, optional, default='fz'
-         Weighting factor for calculating the structure-factor, default is Faber-Ziman.
+    weighting_factor : string, optional, default='fz', other='al'
+         Weighting factor for calculating the structure-factor, default is Faber-Ziman, other option is Ashcroft-Langreth.
         See https://openscholarship.wustl.edu/etd/1358/ and http://isaacs.sourceforge.net/manual/page26_mn.html for details.
 
     Returns
@@ -49,10 +50,13 @@ def structure_factor(trj, Q_range=(0.5, 50), n_points=1000, framewise_rdf=False,
         The structure factor of the trajectory
 
     """
-    if weighting_factor not in ['fz']:
-        raise ValueError('Invalid weighting_factor `{}` is given.'
-                         '  The only weighting_factor currently supported is `fz`.'.format(
-                             weighting_factor))
+    if weighting_factor not in ["fz"]:
+        raise ValueError(
+            "Invalid weighting_factor `{}` is given."
+            "  The only weighting_factor currently supported is `fz`.".format(
+                weighting_factor
+            )
+        )
 
     rho = np.mean(trj.n_atoms / trj.unitcell_volumes)
     L = np.min(trj.unitcell_lengths)
@@ -64,23 +68,28 @@ def structure_factor(trj, Q_range=(0.5, 50), n_points=1000, framewise_rdf=False,
     form_factors = dict()
     rdfs = dict()
 
-    Q = np.logspace(np.log10(Q_range[0]),
-                    np.log10(Q_range[1]),
-                    num=n_points)
+    Q = np.logspace(np.log10(Q_range[0]), np.log10(Q_range[1]), num=n_points)
     S = np.zeros(shape=(len(Q)))
 
     for elem in elements:
-        compositions[elem.symbol] = len(top.select('element {}'.format(elem.symbol)))/trj.n_atoms
+        compositions[elem.symbol] = (
+            len(top.select("element {}".format(elem.symbol))) / trj.n_atoms
+        )
         form_factors[elem.symbol] = elem.atomic_number
 
     for i, q in enumerate(Q):
         num = 0
         denom = 0
 
-        for elem in elements:
-            denom += compositions[elem.symbol] * form_factors[elem.symbol]
-
         for (elem1, elem2) in it.product(elements, repeat=2):
+            denom += _get_normalize(
+                weighting_factor,
+                compositions[elem1.symbol],
+                form_factors[elem1.symbol],
+                compositions[elem2.symbol],
+                form_factors[elem2.symbol],
+            )
+
             e1 = elem1.symbol
             e2 = elem2.symbol
 
@@ -89,36 +98,38 @@ def structure_factor(trj, Q_range=(0.5, 50), n_points=1000, framewise_rdf=False,
 
             x_a = compositions[e1]
             x_b = compositions[e2]
-            
+
             try:
-                g_r = rdfs['{0}{1}'.format(e1, e2)]
+                g_r = rdfs["{0}{1}".format(e1, e2)]
             except KeyError:
-                pairs = top.select_pairs(selection1='element {}'.format(e1),
-                                         selection2='element {}'.format(e2))
+                pairs = top.select_pairs(
+                    selection1="element {}".format(e1),
+                    selection2="element {}".format(e2),
+                )
                 if framewise_rdf:
-                    r, g_r = rdf_by_frame(trj,
-                                         pairs=pairs,
-                                         r_range=(0, L / 2),
-                                         bin_width=0.001)
+                    r, g_r = rdf_by_frame(
+                        trj, pairs=pairs, r_range=(0, L / 2), bin_width=0.001
+                    )
                 else:
-                    r, g_r = md.compute_rdf(trj,
-                                            pairs=pairs,
-                                            r_range=(0, L / 2),
-                                            bin_width=0.001)
-                rdfs['{0}{1}'.format(e1, e2)] = g_r
+                    r, g_r = md.compute_rdf(
+                        trj, pairs=pairs, r_range=(0, L / 2), bin_width=0.001
+                    )
+                rdfs["{0}{1}".format(e1, e2)] = g_r
             integral = simps(r ** 2 * (g_r - 1) * np.sin(q * r) / (q * r), r)
 
-            if weighting_factor == 'fz':
-                pre_factor = 4 * np.pi * rho
-                partial_sq = (integral*pre_factor) + 1
-                num += (x_a*f_a*x_b*f_b) * (partial_sq)
-        S[i] = (num/(denom**2))
+            pre_factor = 4 * np.pi * rho
+            partial_sq = (integral * pre_factor) + 1
+            num += (x_a * f_a * x_b * f_b) * (partial_sq)
+
+        S[i] = num / denom
+
     return Q, S
+
 
 def compute_dynamic_rdf(trj):
     """Compute r_ij(t), the distance between atom j at time t and atom i and
     time 0. Note that this alone is likely useless, but is an intermediate
-    variable in the construction of a dynamic structure factor. 
+    variable in the construction of a dynamic structure factor.
     See 10.1103/PhysRevE.59.623.
 
     Parameters
@@ -140,12 +151,16 @@ def compute_dynamic_rdf(trj):
     for n_frame, frame in enumerate(trj):
         for atom_i in range(trj.n_atoms):
             for atom_j in range(trj.n_atoms):
-                r_ij[atom_i, atom_j, n_frame] = compute_distance(trj.xyz[n_frame, atom_j], trj.xyz[0, atom_i])
+                r_ij[atom_i, atom_j, n_frame] = compute_distance(
+                    trj.xyz[n_frame, atom_j], trj.xyz[0, atom_i]
+                )
 
     return r_ij
 
+
 def compute_distance(point1, point2):
-    return np.sqrt(np.sum((point1 -point2) ** 2))
+    return np.sqrt(np.sum((point1 - point2) ** 2))
+
 
 def compute_rdf_from_partial(trj, r_range=None):
     compositions = dict()
@@ -158,7 +173,9 @@ def compute_rdf_from_partial(trj, r_range=None):
 
     denom = 0
     for elem in elements:
-        compositions[elem.symbol] = len(top.select('element {}'.format(elem.symbol)))/trj.n_atoms
+        compositions[elem.symbol] = (
+            len(top.select("element {}".format(elem.symbol))) / trj.n_atoms
+        )
         form_factors[elem.symbol] = elem.atomic_number
         denom += compositions[elem.symbol] * form_factors[elem.symbol]
     for i, (elem1, elem2) in enumerate(it.product(elements, repeat=2)):
@@ -170,24 +187,30 @@ def compute_rdf_from_partial(trj, r_range=None):
 
         f_a = form_factors[e1]
         f_b = form_factors[e2]
-        
+
         try:
-            g_r = rdfs['{0}{1}'.format(e1, e2)]
+            g_r = rdfs["{0}{1}".format(e1, e2)]
         except KeyError:
-            pairs = top.select_pairs(selection1='element {}'.format(e1),
-                                     selection2='element {}'.format(e2))
+            pairs = top.select_pairs(
+                selection1="element {}".format(e1), selection2="element {}".format(e2)
+            )
             if r_range == None:
-                r, g_r = md.compute_rdf(trj,
-                                        pairs=pairs,
-                                        r_range=(0, L / 2))
+                r, g_r = md.compute_rdf(trj, pairs=pairs, r_range=(0, L / 2))
             else:
-                r, g_r = md.compute_rdf(trj,
-                                        pairs=pairs,
-                                        r_range=r_range)
-            rdfs['{0}{1}'.format(e1, e2)] = g_r
+                r, g_r = md.compute_rdf(trj, pairs=pairs, r_range=r_range)
+            rdfs["{0}{1}".format(e1, e2)] = g_r
         if i == 0:
-            total = g_r * (x_a*x_b*f_a*f_b) / denom**2
-        else: 
-            total += g_r * (x_a*x_b*f_a*f_b) / denom**2
+            total = g_r * (x_a * x_b * f_a * f_b) / denom ** 2
+        else:
+            total += g_r * (x_a * x_b * f_a * f_b) / denom ** 2
 
     return r, total
+
+
+def _get_normalize(method, c_a, f_a, c_b, f_b):
+    """Get normalization factor"""
+    if method == "fz":
+        return (c_a * f_a + c_b * f_b) ** 2
+
+    elif method == "al":
+        return c_a * f_a ** 2 + c_b * f_b ** 2
